fc9aee9d71a7db249dab643947a47315e61bc79d
B1aho <wheelsurgut00@gmail.com>
2024-10-29 11:02:17 +0300

    complete main scenarios for console version

--- script.js
+++ script.js
@@ -39,9 +39,42 @@ const GameField = (function() {
         return 1;
     }
 
+    const checkEnd = (row, col) => {
+        const token = field[row][col].getValue()
+
+        // Check win in a row
+        const row = field[row].filter((el) => el.getValue === token)
+        if (row.length === 3) {
+            return true;
+        }
+
+        // Check win in a column
+        const column = field.filter((el) => el[col].getValue() === token)
+        if (column.length === 3) {
+            return true;
+        }
+
+        // Check win in a diagonals
+        const validDiagonal = [[0,0], [2,2], [0,2], [2,0], [1,1],]
+        const idx = validDiagonal.indexOf([row, col])
+        if (idx >= 0) {
+            const mainDiag = (field[0][0] === field[1][1] === field[2][2]) ? true : false
+            const secondaryDiag = (field[0][2] === field[1][1] === field[2][0])
+            if (idx < 2) {
+                return mainDiag;
+            } else if (idx < 4) {
+                return secondaryDiag;
+            } else {
+                return (mainDiag || secondaryDiag) ? true : false;
+            }
+        }
+    }
+
     return {
         getField,
-
+        makeMove,
+        checkEnd,
+        renderFiled
     }
 })()
 
@@ -63,8 +96,7 @@ const Cell = function() {
 }
 
 const GameControl = (function( playerOne = 'Player One', playerTwo = 'Player Two' ) {
-    const players = 
-    [
+    const players = [
         {
             playerName: playerOne,
             token: 'X'
@@ -74,4 +106,42 @@ const GameControl = (function( playerOne = 'Player One', playerTwo = 'Player Two
             token: 'O'
         }
     ]
+    const field = GameField.getField()
+
+    const activeTurn = players[0]
+    const movesCounter = 0
+
+    const turnMove = () => {
+        activeTurn = activeTurn === players[0] ? players[1] : players[0]
+        movesCounter++
+    }
+
+    const makeMove = () => {
+        const row = prompt('Enter row')
+        const col = prompt('Enter column')
+
+        if (field.makeMove(row, col, activeTurn.token)) {
+            turnMove()
+            field.renderFiled()
+        }
+
+        if (movesCounter > 5) {
+            if (movesCounter === 9) {
+                draw()
+            }
+            const haveWinner = field.checkEnd(row, col)
+            if (haveWinner) {
+                win(activeTurn.token)
+            }
+        }
+    }
+
+    const draw = () => {
+
+    }
+
+    const win = () => {
+
+    }
+
 })() 
